42df4fec03436fd0a4054689363024b3d3c8e8a6
Gavin Kyte <gpkyte@gmail.com>
2019-09-19 16:57:53 -0400

    Adding helpers, account for combined ranges in day
    multiple ranges not satisfied with yet
    slots show up as wrong day because of split, will refactor

--- scheduler.py
+++ scheduler.py
@@ -14,57 +14,69 @@
 # collected and reinterpretted as human readable shifts assigned
 # the original n workers. Ideally all shifts will be matched
 # and each worker has a reasonable shift
-#
-# The UID format is 'T##DMMMM':
-#   T = Type of time slot (shift versus worker)
-#   # = ID of time slot (first worker's id is 00)
-#   D = Day of the week
-#   M = Minutes from 0-1439 (24 * 60 - 1) of given day
 
-def generate_shifts(workers, shifts):
-    interpreter = ScheduleInterpreter()
-    type_id = 1 # For shifts
-    shift_id = 0
-    open_shifts = []
-    
-    for this_shift in shifts:
-        slots = interpreter.flatten_time_ranges(this_shift)
-        open_shifts += [type_id * TYPE_OFFSET + shift_id * ID_OFFSET \
-                        + shift for shift in slots]
-        shift_id += 1
-
-    type_id = 2 # For workers
-    worker_id = shift_id + 1
-    worker_shifts = []
-
-    for this_worker in workers:
-        slots = interpreter.flatten_time_ranges(this_worker)
-        worker_shifts += [type_id * TYPE_OFFSET + worker_id * ID_OFFSET \
-                            + shift for shift in slots]
-        worker_id += 1
-
-class ScheduleInterpreter(self):
-    DOW_OFFSET = 10**3
+class ScheduleInterpreter():
+    DOW_OFFSET = 10000
     SHIFT_LENGTH = 15
+
+    # Creates UID for every possible time slot in week_availability
+    # The UID format is 'T##DMMMM':
+    #   T = Type of time slot (shift versus worker)
+    #   # = ID of time slot (first worker's id is 00)
+    #   D = Day of the week
+    #   M = Minutes from 0-1439 (24 * 60 - 1) of given day
+    def generate_shifts(self, time_sets, type_id):
+        time_set_id = 0
+        shifts = []
+
+        for this_shift in shifts:
+            slots = self.flatten_time_ranges(this_shift)
+            shifts += [type_id * self.TYPE_OFFSET + time_set_id * self.ID_OFFSET \
+                    + shift for shift in slots]
+            shift_id += 1
+
+        return shifts
 
     # Take set of time ranges in human readable format
     # and return list of ranges in integer format
     # Used both to create shifts and worker availability
     def flatten_time_ranges(self, work_hours):
         DOW = 'MTWRFSU'
-        just_times = [work_hours.get(day) for day in DOW]
+        t1_until_t2_list = []
+
+        for day in DOW:
+            possible_csv_of_time_ranges = work_hours.get(day)
+            if possible_csv_of_time_ranges:
+                t1_until_t2_list += possible_csv_of_time_ranges.split(', ')
 
         flat = []
-        for i in range(len(just_times)):
-            left, right = just_times[i].split('-')
+        for i in range(len(t1_until_t2_list)):
+            left, right = t1_until_t2_list[i].split('-')
             # Creating UID which expresses info like weekday + time
             flat += [i * self.DOW_OFFSET + timeblock \
-                        for timeblock in create_time_range(left, right)]
+                        for timeblock in self.create_time_range(left, right)]
 
         return flat
 
 
-    def timecheck(t):
+    def combine_adjacent_slots(self):
+        pass
+
+    def text_to_minutes(self, time):
+        t = self.timecheck(time)
+        hr = int(t.split(':')[0])
+        m = int(t.split(':')[1])
+        return (60 * hr + m)
+
+    def minutes_to_text(self, num):
+        hr = num // 60
+        m = num % 60
+        return("%s:%s" % (hr, m))
+
+    def round_off(self, num, interval):
+        return ((num // interval) * interval)
+
+    def timecheck(self, t):
         # Several conditions exist for now return input
         # pm/am
         # No :mm
@@ -72,22 +84,14 @@ class ScheduleInterpreter(self):
         return t
 
 
-    # Given 13:00, 16:50. return 1300, 1315, 1330, 1345, 1400, 1415, ..., 1645
+    # Given 13:00 and 16:50, return 13*60, 13*60 +15, ..., 16:30
     def create_time_range(self, start_inclusive, end_exclusive):
         interval = self.SHIFT_LENGTH
-        start_inclusive = timecheck(start_inclusive)
-        end_exclusive = timecheck(end_inclusive)
+        starting_minute = self.text_to_minutes(start_inclusive)
+        ending_minute = self.text_to_minutes(end_exclusive)
 
-        hr_1 = int(start_inclusive.split(':')[0])
-        m_1 = int(start_inclusive.split(':')[1])
-        hr_2 = int(end_exclusive.split(':')[0])
-        m_2 = int(end_exclusive.split(':')[1])
-
-        # Round off by interval
-        start_in_minutes = ((hr_1 * 60 + m_1) / interval) * interval
-        end_in_minutes = ((hr_2 * 60 + m_2) / interval) * interval
-        
-        # Converting to min for simplicity, exclude last
+        start_in_minutes = self.round_off(starting_minute, interval)
+        end_in_minutes = self.round_off(ending_minute, interval)
         return range(start_in_minutes, end_in_minutes, interval)
 
 
